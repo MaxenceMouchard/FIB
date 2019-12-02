b2f0e5a25cd6c1957b2713c3f47548bc51a25259
Aurélien CH <aurelien.charpilienne@epitech.eu>
2019-12-02 18:31:02 +0100

    re organize contact function

--- JavaScript.js
+++ JavaScript.js
@@ -119,51 +119,6 @@ function loadNewOrganization(siteId) {
     loadDataPropertyTitles(selectedSite);
 }
 
-function loadDataContact(oSite) {
-    let contactsReferenced = document.getElementById("contactsReferenced");
-    let counter = 0;
-    contactsReferenced.innerHTML = '';
-    Array.from(jsonAllContacts).forEach( (item, index) => {
-        if(item.OrganizationId === oSite.Id) {
-            let separator = (counter > 0) ? '<hr class="contactSeparator"/>' : '';
-            let contactFullName = (item.ContactFullName !== null) ? item.ContactFullName : '';
-            let contactInitial = (contactFullName).split(/\s/).reduce((response,word)=> response += word.slice(0,1), '');
-            let contactJob = (item.ContactJob !== null) ? item.ContactJob : 'Inconnu';
-            let contactMobilePro = (item.ContactMobilePro !== null) ? item.ContactMobilePro : 'Inconnu';
-            let contactLandlinePro = (item.ContactLandlinePro !== null) ? item.ContactLandlinePro : 'Inconnu';
-            let contactEmail = (item.ContactEmail !== null) ? item.ContactEmail : 'Inconnu';
-            let contactLastConnection = (item.ContactLastConnection !== null) ? item.ContactLastConnection : 'Inconnu';
-            let contactConnectionsCounter = (item.ContactConnectionsCounter !== null) ? item.ContactConnectionsCounter : 'Inconnu';
-            contactsReferenced.innerHTML +=
-            separator + '<div class="contactRow">' +
-                '<div class="squareContact">' +
-                    '<div class="contactInitial">' + contactInitial + '</div>' +
-                '</div>' +
-                '<div class="contactNameAndFunction">' +
-                    '<div class="contactName">' + contactFullName + '</div>' +
-                    '<div class="contactFunction">' + contactJob + '</div>' +
-                '</div>' +
-                '<div class="contactPhonesNumbers">' +
-                    '<div><i class="fas fa-phone"></i>' + contactMobilePro + '</div>' +
-                    '<div><i class="fas fa-phone"></i>' + contactLandlinePro + '</div>' +
-                '</div>' +
-                '<div class="contactEmail">' +
-                    '<i class="fas fa-envelope"></i>' + contactEmail +
-                '</div>' +
-                '<div class="contactLog">' +
-                    '<div>Dernière connexion le ' + contactLastConnection + '</div>' +
-                    '<div>' + contactConnectionsCounter + ' connexions au total</div>' +
-                '</div>' +
-            '</div>';
-            counter++;
-        };
-    });
-
-    if (counter === 0) {
-        contactsReferenced.innerHTML = `<div style="font-weight: normal; padding-left: 50px;">Aucun contact n'est référencé</div>`;
-    }
-}
-
 function loadDataBatiment(oSite) {
     // Add all identity section attributes :
 
@@ -217,17 +172,18 @@ function placeMarkerOnFIBGoogleMap(oSite) {
 function loadDataContacts(oSite) {
     let contactsReferenced = document.getElementById("contactsReferenced");
     let counter = 0;
+    contactsReferenced.innerHTML = '';
     Array.from(jsonAllContacts).forEach( (item, index) => {
         if(item.OrganizationId === oSite.Id) {
-            let separator = (index < jsonAllContacts.length && counter > 0) ? '<hr class="contactSeparator"/>' : '';
-            let contactFullName = (item.ContactFullName) ? item.ContactFullName : '';
+            let separator = (counter > 0) ? '<hr class="contactSeparator"/>' : '';
+            let contactFullName = (item.ContactFullName !== null) ? item.ContactFullName : '';
             let contactInitial = (contactFullName).split(/\s/).reduce((response,word)=> response += word.slice(0,1), '');
-            let contactJob = (item.ContactJob) ? item.ContactJob : 'Inconnu';
-            let contactMobilePro = (item.ContactMobilePro) ? item.ContactMobilePro : 'Inconnu';
-            let contactLandlinePro = (item.ContactLandlinePro) ? item.ContactLandlinePro : 'Inconnu';
-            let contactEmail = (item.ContactEmail) ? item.ContactEmail : 'Inconnu';
-            let contactLastConnection = (item.ContactLastConnection) ? item.ContactLastConnection : 'Inconnu';
-            let contactConnectionsCounter = (item.ContactConnectionsCounter) ? item.ContactConnectionsCounter : 'Inconnu';
+            let contactJob = (item.ContactJob !== null) ? item.ContactJob : 'Inconnu';
+            let contactMobilePro = (item.ContactMobilePro !== null) ? item.ContactMobilePro : 'Inconnu';
+            let contactLandlinePro = (item.ContactLandlinePro !== null) ? item.ContactLandlinePro : 'Inconnu';
+            let contactEmail = (item.ContactEmail !== null) ? item.ContactEmail : 'Inconnu';
+            let contactLastConnection = (item.ContactLastConnection !== null) ? item.ContactLastConnection : 'Inconnu';
+            let contactConnectionsCounter = (item.ContactConnectionsCounter !== null) ? item.ContactConnectionsCounter : 'Inconnu';
             contactsReferenced.innerHTML +=
             separator + '<div class="contactRow">' +
                 '<div class="squareContact">' +
@@ -252,6 +208,10 @@ function loadDataContacts(oSite) {
             counter++;
         };
     });
+
+    if (counter === 0) {
+        contactsReferenced.innerHTML = `<div style="font-weight: normal; padding-left: 50px;">Aucun contact n'est référencé</div>`;
+    }
 }
 
 function loadDataPropertyTitles(oSite) {
