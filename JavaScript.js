66796a5d7407cc6b469e424f912b2f4d44442acf
MaxElPadre <clairson.max@gmail.com>
2019-12-02 11:22:31 +0100

    Init for razor files

---
+++ JavaScript.js
@@ -0,0 +1,232 @@
+/* ------ HORTREE PLUGIN ------ */
+// PUE SA MERE
+!function (e, t, r, n) { "use strict"; function i(t) { var r = []; return r.push('<div class="hortree-branch">'), e(t).each(function () { r.push(o(this)) }), r.push("</div>"), r.join("\n") } function o(e) { l++; var t = []; return t.push('<div class="hortree-entry" data-entry-id="' + l + '">'), e.tooltip && "" !== e.tooltip.toString().trim() ? (t.push('<div class="hortree-label hortree-tooltip">'), t.push('<span class="hortree-tooltip-text">' + e.tooltip + "</span>"), t.push(e.description), t.push("</div>")) : t.push('<div class="hortree-label">' + e.description + "</div>"), e.children.length && t.push(i(e.children)), t.push("</div>"), t.join("\n") } function h() { var t = []; e(".hortree-entry").each(function () { var r = e(this).attr("data-entry-id"); t.push({ entryId: parseInt(r), entry: e(this) }) }); var r = t.slice(0); r.sort(function (e, t) { return e.entryId - t.entryId }), r.reverse(); for (var n = 0; n < r.length; n++) { var i = r[n].entry, o = i.children(".hortree-branch"); if (o.length) { var h = 0; o.each(function () { h += e(this).height() }), i.height(h) } } } function a(t) { function r(t) { for (var r = 0, n = 0; null !== t && (r += t.offsetLeft, n += t.offsetTop, t = t.offsetParent, !e(".hortree-wrapper").is(t));); return { x: r, y: n } } e(".hortree-wrapper").each(function () { var n = e(this), i = 0, o = 0; n.find(".hortree-label").each(function () { if (0 === o) { var h = e(this).offset().top; i = -1 * h + 20 } if (e(this).siblings(".hortree-branch").length) { var a = r(e(this).get(0)); e(this).siblings(".hortree-branch").children(".hortree-entry").children(".hortree-label").each(function () { var o = r(e(this).get(0)); n.line(a.x + e(this).width() - 10, a.y + i, o.x, o.y + i, { zindex: t.lineZindex, color: t.lineColor, stroke: t.lineStrokeWidth }) }) } o++ }) }) } function s() { e(".hortree-label").each(function () { var t = e(this).height(); e(this).parent(".hortree-entry").height(t) }) } var l = 0; e.fn.hortree = function (t) { t = t || {}; var r = { lineStrokeWidth: 2, lineZindex: 8, lineColor: "#4b86b7", data: [], onComplete: function () { } }, n = e.extend(r, t); if (!e.fn.line) throw new Error("You must load jquery.line.js library! Get it here: https://github.com/tbem/jquery.line"); if (!n.data) throw new Error("No data specified!"); if (!(n.data instanceof Array)) throw new Error("Data should be an array"); n.data.length || console.warn("Data is empty"); var o = []; o.push('<div class="hortree-wrapper">'), o.push(i(n.data)), o.push("</div>"), this.html(o.join("\n")), s(), h(), a(n), n.onComplete && "function" == typeof n.onComplete && n.onComplete.apply() } }(jQuery, window, document);
+
+/* ------ LINE PLUGIN ------ */
+//PUE SA MERE
+!function (t) { var e = function (t, e, s, l, a) { var n = navigator.userAgent.indexOf("MSIE") > -1; if (s < t) { var r = t; t = s, s = r, r = e, e = l, l = r } var o = document.createElement("div"); o.className = a.class; var i = Math.sqrt((t - s) * (t - s) + (e - l) * (e - l)); if (o.style.width = i + "px", o.style.borderBottom = a.stroke + "px " + a.style, o.style.borderColor = a.color, o.style.position = "absolute", o.style.zIndex = a.zindex, n) { o.style.top = l > e ? e + "px" : l + "px", o.style.left = t + "px"; var c = (s - t) / i, f = (l - e) / i; o.style.filter = "progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=" + c + ", M12=" + -1 * f + ", M21=" + f + ", M22=" + c + ")" } else { var d = Math.atan((l - e) / (s - t)); o.style.top = e + .5 * i * Math.sin(d) + "px", o.style.left = t - .5 * i * (1 - Math.cos(d)) + "px", o.style.transform = o.style.MozTransform = o.style.WebkitTransform = o.style.msTransform = o.style.OTransform = "rotate(" + d + "rad)" } return o }; t.fn.line = function (s, l, a, n, r, o) { return t(this).each(function () { t.isFunction(r) ? (callback = r, r = null) : callback = o, r = t.extend({}, t.fn.line.defaults, r), t(this).append(e(s, l, a, n, r)).promise().done(function () { t.isFunction(callback) && callback.call() }) }) }, t.fn.line.defaults = { zindex: 1e4, color: "#000000", stroke: "1", style: "solid", class: "line" } }(jQuery);
+
+/* ------ CODE ------ */
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Handle the site selection
+            console.log(jsonAllOrganizations)
+    handleSiteSelectorFilter(jsonAllOrganizations);
+
+    // Event when the button for site selection is clicked, data must be updated :
+    document.getElementById("searchFilterButton").addEventListener("click", () => {
+        let selectedValue = document.getElementById("siteSelector").options[document.getElementById("siteSelector").selectedIndex].value;
+        loadNewOrganization(selectedValue, jsonAllOrganizations);
+    });
+    // Event when the button of reboot is clicked, input is cleared :
+    document.getElementById("rebootFilterButton").addEventListener("click", () => {
+        document.getElementById("siteSelector").selectedIndex = 0;
+    });
+
+    // site paris pré-selectionné pour les tests : à supprimer
+    document.getElementById("siteSelector").selectedIndex = 46;
+    $("#searchFilterButton").trigger("click");
+
+    // Add event when user click on openCloseIcon element :
+    handleOpenCloseContainer();
+
+    let jsonLegalDataOne = [
+        {
+            description: "Propriété de Veolia",
+            children: [
+                {
+                    description: "Locataire n°1",
+                    children: [
+                        {
+                            description: "Bale 123",
+                            children: []
+                        },
+                        {
+                            description: "Sous-Location n°2",
+                            children: []
+                        }
+                    ]
+                },
+                {
+                    description: "Altran Services SA",
+                    children: [
+                        {
+                            description: "Bale 123",
+                            children: []
+                        },
+                        {
+                            description: "Bale 99",
+                            children: []
+                        }
+                    ]
+                },
+                {
+                    description: "Altran Services SA",
+                    children: []
+                },
+                {
+                    description: "Altran Services SA",
+                    children: []
+                }
+            ]
+        }
+    ];
+    let jsonLegalDataTwo = [
+        {
+            description: "FR69.P01 FR33.00001 - CMA CGM",
+            children: [
+                {
+                    description: "Altran Services SA",
+                    children: []
+                },
+                {
+                    description: "Altran Services SA",
+                    children: []
+                }
+            ]
+        }
+    ];
+
+    // Horizontal tree creation in Legal Section :
+    createLegalHortree([{
+        horTreeId: "#legalHorTreeOne",
+        horTreeData: jsonLegalDataOne
+    }, {
+        horTreeId: "#legalHorTreeTwo",
+        horTreeData: jsonLegalDataTwo
+    }]);
+});
+
+function handleSiteSelectorFilter(jsonAllOrganizations) {
+    let siteSelector = document.getElementById("siteSelector");
+
+    // Adding all the sites in the selector :
+    jsonAllOrganizations.forEach(element => {
+        let option = document.createElement("option");
+        
+        option.value = element.Id;
+        option.innerHTML = element.Name;
+        siteSelector.appendChild(option);
+    });
+}
+
+function loadNewOrganization(siteId, siteArray) {
+    let selectedSite;
+
+    // Finding the object of the selected site, to get all of his informations :
+    for (let i = 0; i < siteArray.length; i += 1) {
+        if (siteArray[i].Id == siteId)
+            selectedSite = siteArray[i];
+    }
+    Array.from(document.getElementsByClassName("batimentName")).forEach((label) => {
+        label.innerHTML = selectedSite.Name;
+    });
+
+    // Add all identity section attributes :
+    document.getElementById("identityType").innerHTML = selectedSite.CollectionsLocalizedName;
+    document.getElementById("identityStreetLocation").innerHTML = selectedSite.AddressLine1;
+    document.getElementById("identityPostCodeLocation").innerHTML = selectedSite.PostCode;
+    if (selectedSite.PostCode != null && selectedSite.PostCode != undefined)
+        document.getElementById("identityPostCodeLocation").innerHTML += "&nbsp";
+    document.getElementById("identityCityLocation").innerHTML = selectedSite.City;
+    document.getElementById("identityCountryLocation").innerHTML = selectedSite.CountryName;
+    if (selectedSite.M2 != null && selectedSite.M2 != undefined)
+        document.getElementById("identityTotalSurfaceData").innerHTML = selectedSite.M2 + "m²";
+    else
+        document.getElementById("identityTotalSurfaceData").innerHTML = "0m²";
+
+    // Add localisation marker on Google Map :
+    placeMarkerOnFIBGoogleMap(initFIBGoogleMap(), selectedSite);
+}
+
+function placeMarkerOnFIBGoogleMap(identityGoogleMap, selectedSite) {
+    let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(selectedSite.Lat, selectedSite.Long),
+        map: identityGoogleMap
+    });
+
+    identityGoogleMap.panTo(marker.position);
+    identityGoogleMap.setZoom(9);
+}
+
+function handleOpenCloseContainer() {
+    let iconOpenContainer = document.querySelectorAll(".openCloseIcon");
+
+    iconOpenContainer.forEach(element => {
+        element.addEventListener("click", () => {
+            let nextPartContainer = element.parentNode.nextElementSibling;
+
+            if (nextPartContainer.classList.contains("active")) {
+                nextPartContainer.classList.remove("active");
+                element.classList.replace('fa-window-minimize', 'fa-plus');
+            } else {
+                nextPartContainer.classList.add("active");
+                element.classList.replace('fa-plus', 'fa-window-minimize');
+            }
+        });
+    });
+}
+
+function createLegalHortree(arrayHorTreeObjects) {
+    // Creation of the HorTrees
+    arrayHorTreeObjects.forEach((object) => {
+        $(object.horTreeId).hortree({
+            data: object.horTreeData
+        });
+    });
+
+    let horTreeContainers = document.getElementsByClassName("legalHorTreeContainer");
+
+    // The marginTop of the lines of the new HorTree == marginTop of lines of previous HorTree +
+    // height of previous HorTree + marginTop of previous HorTree
+    Array.from(horTreeContainers).forEach((container, index) => {
+        if (index < horTreeContainers.length - 1) {
+            let horTreeLineMarginTop = parseInt(getComputedStyle(document.querySelector("#" + container.id + " .line")).marginTop);
+            let horTreeHeight = parseInt(getComputedStyle(container).height);
+            let horTreeMarginTop = parseInt(getComputedStyle(container).marginTop);
+            let nextHorTreeLineMarginTop = horTreeLineMarginTop + horTreeHeight + horTreeMarginTop;
+
+            Array.from(document.querySelectorAll("#" + horTreeContainers[index + 1].id + " .line")).forEach((line) => {
+                line.style.marginTop = nextHorTreeLineMarginTop.toString() + "px";
+            });
+        }
+    });
+
+    // Text format of the labels
+    Array.from(document.getElementsByClassName("hortree-label")).forEach((label) => {
+        label.classList.add("simpleText");
+    });
+
+    // Managing background color of the labels (depending of their section) + the tooltips only for the first section
+    $(".hortree-label").tooltip({
+        html: true,
+        title: "<div class='simpleText'>" +
+            "Surface totale Propriétaire : <strong>1 000 m²</strong><br />" +
+            "Montant d'acquisition : <strong>12 000 000 €</strong><br />" +
+            "Date d'acquisition : <strong>07/05/2001</strong></div>",
+        placement: "top",
+        container: "body"
+    })
+    Array.from(horTreeContainers).forEach((container, index) => {
+        container.querySelectorAll(".hortree-branch").forEach((branch, indexBis) => {
+            if (indexBis === 0) {
+                branch.querySelectorAll(".hortree-entry > .hortree-label").forEach((label) => {
+                    if (index === 0)
+                        label.style.backgroundColor = "#79D6CB";
+                    else if (index === 1)
+                        label.style.backgroundColor = "#7BD679";
+                })
+            } else if (indexBis === 1) {
+                branch.querySelectorAll(".hortree-entry > .hortree-label").forEach((label) => {
+                    $(label).tooltip("disable");
+                    label.style.backgroundColor = "#76BA74";
+                })
+            } else if (indexBis > 1) {
+                branch.querySelectorAll(".hortree-entry > .hortree-label").forEach((label) => {
+                    $(label).tooltip("disable");
+                    label.style.backgroundColor = "#548852";
+                })
+            }
+        });
+    });
+}
