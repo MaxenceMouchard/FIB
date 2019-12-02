1cef15862f80fb07da251fb7bb2fca5e853a3854
MaxElPadre <clairson.max@gmail.com>
2019-12-02 11:51:48 +0100

    cleaning charpi shit

--- JavaScript.js
+++ JavaScript.js
@@ -7,21 +7,68 @@
 !function (t) { var e = function (t, e, s, l, a) { var n = navigator.userAgent.indexOf("MSIE") > -1; if (s < t) { var r = t; t = s, s = r, r = e, e = l, l = r } var o = document.createElement("div"); o.className = a.class; var i = Math.sqrt((t - s) * (t - s) + (e - l) * (e - l)); if (o.style.width = i + "px", o.style.borderBottom = a.stroke + "px " + a.style, o.style.borderColor = a.color, o.style.position = "absolute", o.style.zIndex = a.zindex, n) { o.style.top = l > e ? e + "px" : l + "px", o.style.left = t + "px"; var c = (s - t) / i, f = (l - e) / i; o.style.filter = "progid:DXImageTransform.Microsoft.Matrix(sizingMethod='auto expand', M11=" + c + ", M12=" + -1 * f + ", M21=" + f + ", M22=" + c + ")" } else { var d = Math.atan((l - e) / (s - t)); o.style.top = e + .5 * i * Math.sin(d) + "px", o.style.left = t - .5 * i * (1 - Math.cos(d)) + "px", o.style.transform = o.style.MozTransform = o.style.WebkitTransform = o.style.msTransform = o.style.OTransform = "rotate(" + d + "rad)" } return o }; t.fn.line = function (s, l, a, n, r, o) { return t(this).each(function () { t.isFunction(r) ? (callback = r, r = null) : callback = o, r = t.extend({}, t.fn.line.defaults, r), t(this).append(e(s, l, a, n, r)).promise().done(function () { t.isFunction(callback) && callback.call() }) }) }, t.fn.line.defaults = { zindex: 1e4, color: "#000000", stroke: "1", style: "solid", class: "line" } }(jQuery);
 
 /* ------ CODE ------ */
+let jsonLegalDataOne = [
+    {
+        description: "Propriété de Veolia",
+        children: [
+            {
+                description: "Locataire n°1",
+                children: [
+                    {
+                        description: "Bale 123",
+                        children: []
+                    },
+                    {
+                        description: "Sous-Location n°2",
+                        children: []
+                    }
+                ]
+            },
+            {
+                description: "Altran Services SA",
+                children: [
+                    {
+                        description: "Bale 123",
+                        children: []
+                    },
+                    {
+                        description: "Bale 99",
+                        children: []
+                    }
+                ]
+            },
+            {
+                description: "Altran Services SA",
+                children: []
+            },
+            {
+                description: "Altran Services SA",
+                children: []
+            }
+        ]
+    }
+];
+
+let jsonLegalDataTwo = [
+    {
+        description: "FR69.P01 FR33.00001 - CMA CGM",
+        children: [
+            {
+                description: "Altran Services SA",
+                children: []
+            },
+            {
+                description: "Altran Services SA",
+                children: []
+            }
+        ]
+    }
+];
 
 document.addEventListener("DOMContentLoaded", () => {
     // Handle the site selection
-            console.log(jsonAllOrganizations)
+    console.log(jsonAllOrganizations);
     handleSiteSelectorFilter(jsonAllOrganizations);
-
-    // Event when the button for site selection is clicked, data must be updated :
-    document.getElementById("searchFilterButton").addEventListener("click", () => {
-        let selectedValue = document.getElementById("siteSelector").options[document.getElementById("siteSelector").selectedIndex].value;
-        loadNewOrganization(selectedValue, jsonAllOrganizations);
-    });
-    // Event when the button of reboot is clicked, input is cleared :
-    document.getElementById("rebootFilterButton").addEventListener("click", () => {
-        document.getElementById("siteSelector").selectedIndex = 0;
-    });
 
     // site paris pré-selectionné pour les tests : à supprimer
     document.getElementById("siteSelector").selectedIndex = 46;
@@ -29,63 +76,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // Add event when user click on openCloseIcon element :
     handleOpenCloseContainer();
-
-    let jsonLegalDataOne = [
-        {
-            description: "Propriété de Veolia",
-            children: [
-                {
-                    description: "Locataire n°1",
-                    children: [
-                        {
-                            description: "Bale 123",
-                            children: []
-                        },
-                        {
-                            description: "Sous-Location n°2",
-                            children: []
-                        }
-                    ]
-                },
-                {
-                    description: "Altran Services SA",
-                    children: [
-                        {
-                            description: "Bale 123",
-                            children: []
-                        },
-                        {
-                            description: "Bale 99",
-                            children: []
-                        }
-                    ]
-                },
-                {
-                    description: "Altran Services SA",
-                    children: []
-                },
-                {
-                    description: "Altran Services SA",
-                    children: []
-                }
-            ]
-        }
-    ];
-    let jsonLegalDataTwo = [
-        {
-            description: "FR69.P01 FR33.00001 - CMA CGM",
-            children: [
-                {
-                    description: "Altran Services SA",
-                    children: []
-                },
-                {
-                    description: "Altran Services SA",
-                    children: []
-                }
-            ]
-        }
-    ];
 
     // Horizontal tree creation in Legal Section :
     createLegalHortree([{
@@ -103,28 +93,46 @@ function handleSiteSelectorFilter(jsonAllOrganizations) {
     // Adding all the sites in the selector :
     jsonAllOrganizations.forEach(element => {
         let option = document.createElement("option");
-        
         option.value = element.Id;
         option.innerHTML = element.Name;
         siteSelector.appendChild(option);
     });
+
+    // Event when the button for site selection is clicked, data must be updated :
+    document.getElementById("searchFilterButton").addEventListener("click", () => {
+        let selectedValue = document.getElementById("siteSelector").options[document.getElementById("siteSelector").selectedIndex].value;
+        loadNewOrganization(selectedValue);
+    });
+
+    // Event when the button of reboot is clicked, input is cleared :
+    document.getElementById("rebootFilterButton").addEventListener("click", () => {
+        document.getElementById("siteSelector").selectedIndex = 0;
+    });
 }
 
-function loadNewOrganization(siteId, siteArray) {
-    let selectedSite;
+function loadNewOrganization(siteId) {
+    let selectedSite = {};
 
     // Finding the object of the selected site, to get all of his informations :
-    for (let i = 0; i < siteArray.length; i += 1) {
-        if (siteArray[i].Id == siteId)
-            selectedSite = siteArray[i];
+    for (let i = 0; i < jsonAllOrganizations.length; i += 1) {
+        if (jsonAllOrganizations[i].Id == siteId) {
+            selectedSite = jsonAllOrganizations[i];
+            break;
+        }
     }
+
+    loadDataBatiment(selectedSite);
+}
+
+function loadDataBatiment(oSite) {
+    // Add all identity section attributes :
     Array.from(document.getElementsByClassName("batimentName")).forEach((label) => {
         label.innerHTML = selectedSite.Name;
     });
-
-    // Add all identity section attributes :
     document.getElementById("identityType").innerHTML = selectedSite.CollectionsLocalizedName;
     document.getElementById("identityStreetLocation").innerHTML = selectedSite.AddressLine1;
+    // document.getElementById("identityStreetLocation").innerHTML = selectedSite.AddressLine2;
+    // document.getElementById("identityStreetLocation").innerHTML = selectedSite.AddressLine3;
     document.getElementById("identityPostCodeLocation").innerHTML = selectedSite.PostCode;
     if (selectedSite.PostCode != null && selectedSite.PostCode != undefined)
         document.getElementById("identityPostCodeLocation").innerHTML += "&nbsp";
